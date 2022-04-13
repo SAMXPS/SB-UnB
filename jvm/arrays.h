ad4f7afdaff588e7234ddeaa89ef57491ba868b2
Samuel James <samuel.jlbsb@gmail.com>
2022-04-13 09:24:35 -0300

    Arrays mais simples

--- jvm/arrays.h
+++ jvm/arrays.h
@@ -1,22 +1,17 @@
 #include "class/class_definitions.h"
+#include <stdlib.h>
 
 typedef struct {
-    u4                  len;        // tamanho total da array em quantidade de elementos
-    u1                  el_len;     // tamanho em bytes de cada elemento
-    vlen_array_slice*   start;      // início do array
+    u4 len;        // tamanho total da array em quantidade de elementos
+    u1 el_len;     // tamanho em bytes de cada elemento
+    u1 data[];     // início do array
 } vlen_array;
 
-typedef struct {
-    u2                  len;        // tamanho do slice atual, em quantidade de elementos
-    vlen_array_slice*   next;       // endereço do próximo slice
-    u1                  data[];     // dados desse slice (tamanho variável)
-} vlen_array_slice;
-
-void* vlen_array_get_element(vlen_array* array, u2 pos) {
-    // procurar slices até achar a posição correta e retornar
-    // ponteiro pro endereço do elemento...
-}
-
 vlen_array* vlen_array_alloc(u4 len, u1 el_len) {
-    // alocar um array de tamanho variável
+    vlen_array* array = (vlen_array*) malloc(sizeof(vlen_array)+len*el_len);
+    if (array) {
+        array->len = len;
+        array->el_len = el_len;
+    }
+    return array;
 }
