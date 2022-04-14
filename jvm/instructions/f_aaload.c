9dc15c9b1d0d72428f594383f1753ca8e645156e
Samuel James <samuel.jlbsb@gmail.com>
2022-04-14 11:48:16 -0300

    Implementando aaload

--- jvm/instructions/f_aaload.c
+++ jvm/instructions/f_aaload.c
@@ -1,5 +1,19 @@
 ﻿#include "base_instruction.h"
+#include "arrays.h"
 
-f_aaload() {
-	//todo
+void f_aaload() {
+	vlen_array* array = i_pop(vlen_array*);
+	if (!array) {
+		i_throw(NullPointerException);
+		return;
+	}
+
+	int index = i_pop(int);
+
+	if (index < 0 || index > array->len) {
+		i_throw(ArrayIndexOutOfBoundsException);
+		return;
+	}
+
+	i_push(array->data);
 }
